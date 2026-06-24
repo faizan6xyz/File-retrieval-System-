f80258074b52d01dcb48ac261509501f256ad289
faizan6xyz <232054644+faizan6xyz@users.noreply.github.com>
2026-06-24 12:08:56 +0530

    condition for the if not found

--- SYSTEM/Image_tobase.py
+++ SYSTEM/Image_tobase.py
@@ -1,5 +1,10 @@
 import base64
-def imageconvert (name):
-    with open("name", "rb") as f:
+import os 
+def imageconvert (name, folder_path = "SYSTEM/Data"):
+    file_name = os.path.join(folder_path , name)
+    if not os.path.exists(file_name):
+        print(f"{file_name} not found ")
+        return None
+    with open(file_name , "rb") as f:
         image_b64 = base64.b64encode(f.read()).decode("utf-8")
     return image_b64
--- SYSTEM/image_to_tensorandarray.py
+++ SYSTEM/image_to_tensorandarray.py
@@ -1,9 +1,14 @@
 import torch
 from PIL import Image
 import numpy as np
-def image_to_tensor(image_path, target_size=(336, 336)):
+import os 
+def image_to_tensor(name ,folder_path = "SYSTEM?data",target_size=(336, 336)):
+    file_name = os.path.join(folder_path , name)
+    if not os.path.exists(file_name):
+        print(f"{file_name} not found ")
+        return None
     # 1. Load the image using Pillow
-    img = Image.open(image_path).convert("RGB")
+    img = Image.open(file_name).convert("RGB")
     # 2. Resize the image (Vision encoders usually require fixed sizes like 336x336)
     img = img.resize(target_size, Image.Resampling.LANCZOS)
     # 3. Convert to NumPy array (Height, Width, Channels)
@@ -15,5 +20,6 @@ def image_to_tensor(image_path, target_size=(336, 336)):
     # 6. Add a "batch" dimension ,  Models expect [Batch_Size, Channels, Height, Width] -> [1, 3, 336, 336]
     img_tensor = img_tensor.unsqueeze(0)
     return img_tensor
-tensor = image_to_tensor("screenshot.png")
-print(f"Tensor Shape: {tensor.shape}") 
+if __name__ == "__main__":  
+    tensor = image_to_tensor("screenshot.png")
+    print(f"Tensor Shape: {tensor.shape}") 
